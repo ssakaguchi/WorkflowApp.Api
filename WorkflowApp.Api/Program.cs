f2198a6bffbc29246d4817bfc9120250df7c5007
Seiya Sakaguchi <s4e2i4ya@gmail.com>
2026-03-20 18:07:46 +0900

    Swagger

--- WorkflowApp.Api/Program.cs
+++ WorkflowApp.Api/Program.cs
@@ -64,6 +64,8 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
